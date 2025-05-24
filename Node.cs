6fc672c9b159ef6d087738d90a985e8abc96ecd7
ShashiniWanninayake <shashinethma15@gmail.com>
2025-05-24 17:30:09 +0530

    Add files via upload

---
+++ Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    class Node
+    {
+        public Student Data { get => Data; set => Data = value; }
+        public Node Next { get=> Next; set=>Next = value; }
+
+        public Node(Student student)
+        {
+            Data = student;
+            Next = null;
+        }
+    }
+
+
+
+
+
+}
---
+++ Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    class Student
+    {
+        public int IndexNumber { get; set; }
+        public string Name { get; set; }
+        public double GPA { get; set; }
+        public int AdmissionYear { get; set; }
+        public string NIC { get; set; }
+
+        public override string ToString()
+        {
+            return $"Index: {IndexNumber} | Name: {Name} | GPA: {GPA:F2} | Admission Year: {AdmissionYear} | NIC: {NIC}";
+        }
+    }
+}
